ac138afb7af03476102983c088cd3cb377493e81
Li Zhong <floridsleeves@gmail.com>
2023-11-06 01:16:28 -0800

    remove

--- checker/src/checker/model/APISeqItem.java
+++
@@ -1,5 +0,0 @@
-package checker.model;
-
-public interface APISeqItem {
-
-}
--- checker/src/checker/model/ViolationType.java
+++
@@ -1,9 +0,0 @@
-package checker.model;
-
-public enum ViolationType {
-	IncorrectPrecondition,
-	MissingMethodCall,
-	MissingStructure,
-	DisorderMethodCall,
-	DisorderStructure
-}
